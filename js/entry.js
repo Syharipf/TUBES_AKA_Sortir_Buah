69dc7a72f549d1fcb63ea4286ef6b054e389be85
syharipf <email.syharipf@gmail.com>
2025-12-18 23:32:05 +0700

    feat(entry): Menangani input buah, Validasi, dan penyimpanan data di localStorage

---
+++ js/entry.js
@@ -0,0 +1,72 @@
+// =========================
+// STATE
+// =========================
+let dataBuah = JSON.parse(localStorage.getItem("dataBuah")) || [];
+
+// =========================
+// AMBIL ELEMENT
+// =========================
+const buahEl = document.getElementById("buah");
+const beratEl = document.getElementById("berat");
+const listEl = document.getElementById("listEntry");
+
+const tambahBtn = document.getElementById("tambahBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+// =========================
+// RENDER
+// =========================
+function render() {
+  listEl.innerHTML = "";
+
+  if (dataBuah.length === 0) {
+    listEl.innerHTML = `
+      <tr>
+        <td colspan="3" style="text-align:center;color:#777;">
+          Belum ada data
+        </td>
+      </tr>
+    `;
+    return;
+  }
+
+  dataBuah.forEach((item, i) => {
+    listEl.innerHTML += `
+      <tr>
+        <td>${i + 1}</td>
+        <td>${item.buah}</td>
+        <td>${item.berat.toFixed(2)}</td>
+      </tr>
+    `;
+  });
+
+  // SIMPAN SETIAP PERUBAHAN
+  localStorage.setItem("dataBuah", JSON.stringify(dataBuah));
+}
+
+// =========================
+// EVENT
+// =========================
+tambahBtn.addEventListener("click", () => {
+  if (!buahEl.value || beratEl.value <= 0) return;
+
+  dataBuah.push({
+    buah: buahEl.value,
+    berat: parseFloat(beratEl.value)
+  });
+
+  buahEl.value = "";
+  beratEl.value = "";
+  render();
+});
+
+resetBtn.addEventListener("click", () => {
+  dataBuah = [];
+  localStorage.removeItem("dataBuah");
+  render();
+});
+
+// =========================
+// INIT
+// =========================
+render();
