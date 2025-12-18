b188df7e0582edbde8f9d770ea6db22b3594f555
syharipf <email.syharipf@gmail.com>
2025-12-18 23:30:03 +0700

    feat(data): mendefinisikan data harga buah

---
+++ js/data.js
@@ -0,0 +1,16 @@
+const hargaBuah = {
+  pisang: 25000,
+  jeruk: 28000,
+  mangga: 35000,
+  apel: 35000,
+  nanas: 30000,
+  anggur: 45000,
+  pir: 30000,
+  semangka: 20000,
+  salak: 20000,
+  alpukat: 28000,
+  jambu: 20000,
+  naga: 22000,
+  durian: 35000
+};
+    
